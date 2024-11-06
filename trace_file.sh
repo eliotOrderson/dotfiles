f8dac587b27f3f9e6239b86f0a1b342a94d05b00
eliot <elitotder@gmail.com>
2024-11-06 12:13:42 +0800

    nothing

--- trace_file.sh
+++ trace_file.sh
@@ -2,20 +2,23 @@
 
 # trace array
 trace_array=(
-  "$HOME/.config/kitty/kitty.conf"
-  "$HOME/.config/hypr/userprefs.conf"
-  "$HOME/.config/fcitx5"
-  "$HOME/.local/share/bin/windowoperation.sh"
-  "$HOME/.local/share/bin/safe_rm.sh"
-  "$HOME/.local/share/bin/autostart.sh"
-  "$HOME/.zshrc"
-  "$HOME/trace_file.sh"
+    "$HOME/.config/pip/pip.conf"
+    "$HOME/.config/neovide/config.toml"
+    "$HOME/.config/nvim/"
+    "$HOME/.config/kitty/kitty.conf"
+    "$HOME/.config/hypr/userprefs.conf"
+    "$HOME/.config/fcitx5"
+    "$HOME/.local/share/bin/windowoperation.sh"
+    "$HOME/.local/share/bin/safe_rm.sh"
+    "$HOME/.local/share/bin/autostart.sh"
+    "$HOME/.zshrc"
+    "$HOME/trace_file.sh"
 )
 
 # concatenate the trace array into a string
 trace_string=$(
-  IFS=" "
-  echo "${trace_array[*]}"
+    IFS=" "
+    echo "${trace_array[*]}"
 )
 
 mv $HOME/dotfiles/.git $HOME
@@ -24,7 +27,7 @@ git commit -m "nothing"
 mv .git $HOME/dotfiles -f
 
 if [ $? -eq 0 ]; then
-  echo "Files added successfully."
+    echo "Files added successfully."
 else
-  echo "Error adding files."
+    echo "Error adding files."
 fi
