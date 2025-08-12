8f176e512c349b22659aa7ed0795189b5aa7dace
eliot <eliotorderson@gmail.com>
2025-08-12 17:30:18 +0800

    auto commit

--- .local/share/bin/hypr_dotfiles_tracing.sh
+++ .local/share/bin/hypr_dotfiles_tracing.sh
@@ -8,6 +8,8 @@ trace_array=(
     "$HOME/.config/zed/"
     "$HOME/.config/kitty/kitty.conf"
     "$HOME/.config/hypr/userprefs.conf"
+    "$HOME/.config/user-dirs.dirs"
+    "$HOME/.config/user-dirs.locale"
 
     "$HOME/.config/fcitx5"
     "$HOME/.local/share/fcitx5"
